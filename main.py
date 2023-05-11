fc556914e29dd3e383d306f1e0362ce2bf84d8fe
Mohamed Elsharaky <101217349+Elsharaky@users.noreply.github.com>
2023-05-11 17:24:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def docNormalization(doc: str) -> str:
     return ''.join([c.lower() for c in list(doc) if c.isalpha() or c.isspace()])
+def removeStopWords(doc: str) -> str:
+    with open('stopwords.txt') as f:
+        stopWords = f.readlines()
+        for i in range(len(stopWords)):
+            stopWords[i] = stopWords[i][:-1]
+    return ''.join([word for word in doc.split() if word not in stopWords])
 
 def expNormalization(exp: str) -> str:
     exp = ''.join([c.lower() for c in exp if c.isalpha() or c.isspace() or c in ['!','(',')','&','|']])
@@ -126,7 +132,7 @@ def main() -> None:
             print('Invalid input try again!')
             continue
         docs = [input(f'Enter document {i+1}: ').strip() for i in range(int(nDocs))]
-        docs = [docNormalization(d) for d in docs]
+        docs = [removeStopWords(docNormalization(d)) for d in docs]
         expresion = input('Enter your query: ').strip()
         expresion = infix2postfix(expNormalization(expresion))
         while (choice := input('Enter 1 for Boolean Retrieval or 2 for Inverted Index: ').strip()) not in ('1','2'):
